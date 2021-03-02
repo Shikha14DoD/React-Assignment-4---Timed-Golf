c5153b0a14effbf1a4b4683554aebd4da2a40d51
Shikha14DoD <34702351+Shikha14DoD@users.noreply.github.com>
2021-03-02 15:42:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 ,btnClicked:false,timer:0};
+    this.state = { time: 0, x: 0, y: 0 ,gameHasStarted:true,timer:0,reachedTodestiny:false};
     
      this.keyHandling=this.keyHandling.bind(this);
         this.buttonIsClicked=this.buttonIsClicked.bind(this);
@@ -14,6 +14,7 @@ class Timer extends React.Component {
         
          if(this.state.x===250 && this.state.y===250)
         {
+           this.setState({gameHasStarted:false})
             clearInterval(this.state.timer)
             
         }
@@ -54,12 +55,13 @@ class Timer extends React.Component {
   }
 
    buttonIsClicked(){
-
+ if(this.state.gameHasStarted===true)
+ {
         this.state.timer=setInterval(()=>{
             this.state.time=this.state.time+1
             this.setState({time:this.state.time});
         },1000)
-       
+ }
         
       }
 
